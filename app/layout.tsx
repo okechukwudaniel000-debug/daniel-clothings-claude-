80ef9ab5fcfa2489ddb830c79825b9ab26ca2eb6
Okechukwu Daniel <okechukwudaniel000@gmail.com>
2026-07-18 20:34:48 -0700

    fixed all lint warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,7 @@ export default function RootLayout({
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Sora:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
